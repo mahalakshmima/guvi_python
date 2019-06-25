7de3ff9ce794fbf35459b53870eadb553cdc3399
AbunAbi03 <51960067+AbunAbi03@users.noreply.github.com>
2019-06-25 22:05:28 +0530

    Update leap year r not.py

--- leap year r not.py
+++ leap year r not.py
@@ -1,9 +1,9 @@
 year=int(input())
 if year%4==0:
-  print("leap year")
+  print("yes")
 elif year%100==0:
-  print("not leap year")
+  print("no")
 elif year%400==0:
-  print("leap year")
+  print("yes")
 else:
-  print("not leap year")
+  print("no")
