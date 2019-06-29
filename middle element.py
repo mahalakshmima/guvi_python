c219e61f1b873b85124a34f79e58d7443da6d3c0
AbunAbi03 <51960067+AbunAbi03@users.noreply.github.com>
2019-06-29 16:41:42 +0530

    Update middle element.py

--- middle element.py
+++ middle element.py
@@ -1,5 +1,6 @@
 a=int(input())
-b=list(map(int,input().split()))
-b.sort()
-c = int((len(b) - 1)/2)
-print (b[c])
+if 1<=a<=100000:
+  b=list(map(int,input().split()))
+  b.sort()
+  c = int((len(b) - 1)/2)
+  print (b[c])
